36029c37da3c9da6ab67425b7d9e93b942ef9be5
Korovay4ik <sashakorot1998@mail.ru>
2024-04-30 16:10:01 +0500

    Изменение каталога сборки в bin

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
-#include <QDir>
+//#include <QDir>
+#include <QFile>
+#include <fstream>
+#include <QString>
+#include <QDebug>
+#include <QProcess>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +23,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_upload_user_code_button_clicked()
 {
-    QDir::currentPath();
+    QString file_path = QDir::currentPath();
+    QString user_code = ui->user_code_text_edit->toPlainText();
+
+    //qDebug() << file_path;
+    //std::ofstream file("C:\\Users\\sasha\\Documents\\repositories\\ARM_QT\\workspace\\main_code.cpp");
+    //file.close();
 }
 
